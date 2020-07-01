681013755eaf33481b9f8d5d1f6939ff3d2dd8bb
John Deutscher <johndeu@microsoft.com>
2020-07-01 14:20:46 -0700

    rebuilt

--- rtsp-ws-server/start.sh
+++ rtsp-ws-server/start.sh
@@ -19,7 +19,7 @@
 # To use a different resolution, use a caps filter in the launch pipeline, e.g.:
 #   'videotestsrc ! video/x-raw,width=1280,height=720 ! x264enc ! rtph264pay name=pay0 pt=96'
 
-h264_pipeline="videotestsrc ! video/x-raw,width=1920,height=1080 ! timeoverlay text='H.264' valignment=top halignment=left ! x264enc ! rtph264pay name=pay0 pt=96"
+h264_pipeline="videotestsrc pattern=ball ! video/x-raw,width=320,height=240 ! timeoverlay text='H.264' valignment=top halignment=left ! x264enc ! rtph264pay name=pay0 pt=96"
 h264_port="8554"
 mjpeg_pipeline="videotestsrc pattern=ball ! video/x-raw,width=1280,height=720 ! timeoverlay text='MJPEG' valignment=top halignment=left ! jpegenc ! rtpjpegpay name=pay0 pt=96"
 mjpeg_port="8555"
